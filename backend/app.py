6a4af2f88096e7ac9f8c46d7f686329711baa554
Klayvert2003 <klayvert.ryan16@gmail.com>
2023-09-28 20:57:20 -0300

    [FIX]: Ajustando rotas para seus devidos métodos e ajustando retornos

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 from wordcloud import WordCloud
 from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS
 
 from bot import Bot
 from database.db_controller import SqliteController
 
 app = Flask(__name__)
+CORS(app)
 bot = Bot()
 db = SqliteController()
 nlp = spacy.load("pt_core_news_md")
 
-@app.get('/api/chat-bot')
+@app.post('/api/chat-bot')
 def chat_bot():
     data = request.get_json()
     if data.get('question'):
@@ -27,14 +29,13 @@ def chat_bot():
     else:
         return jsonify({'error': 'question not found'}), 404
 
-@app.get('/api/verify-danger')
+@app.post('/api/verify-danger')
 def verify_danger():
     data = request.get_json()
-    question = data.get('question')
     start_date = data.get('start_date')
     end_date = data.get('end_date')
     
-    if question and start_date and end_date:
+    if start_date and end_date:
         if len(start_date) != 10 or len(end_date) != 10:
             return jsonify({'error': 'invalid date format, correct: YYYY-MM-DD'})
         else:
@@ -46,11 +47,12 @@ def verify_danger():
                     'warning_message': 'Este satélite é potencialmente perigoso para a Terra' if item.get('is_potentially_hazardous') else 'Este satélite não é perigoso para a Terra'
                 }
                 list_responses.append(response_dict)
+            list_responses = [list_responses[0], list_responses[1], list_responses[2]]
             return jsonify({'response': list_responses}), 200
     else:
-        return jsonify({'error': 'question not found'}), 404
+        return jsonify({'error': 'No data found'}), 404
     
-@app.route('/api/generate-word-cloud')
+@app.get('/api/generate-word-cloud')
 def generate_word_cloud():
     responses = db.return_word_clouds()
     text = "\n".join(responses)
@@ -58,7 +60,7 @@ def generate_word_cloud():
     filtered_words = [token.text for token in doc if not token.is_stop]
     
     filtered_text = ' '.join(filtered_words)
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(filtered_text)
+    wordcloud = WordCloud(width=800, height=600, background_color='white').generate(filtered_text)
 
     img_buffer = BytesIO()
     plt.figure(figsize=(8, 4))
